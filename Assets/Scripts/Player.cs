713413a22ba4da6ea230d20619c6c3057dc89d25
Unknown <c.jeauc@student.isartdigital.com>
2018-01-28 11:12:28 -0500

    adding player sprite and changing on movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,7 +9,27 @@ public class Player : MonoBehaviour
     private string inputAxisV = "Vertical";
 
     [SerializeField]
+    private SpriteRenderer m_renderer;
+
+    [SerializeField]
+    private Sprite facing_right;
+
+    [SerializeField]
+    private Sprite facing_left;
+
+    [SerializeField]
+    private Sprite facing_up;
+
+    [SerializeField]
+    private Sprite facing_down;
+
+    [SerializeField]
     private float speed = 0.1f;
+
+    void Start()
+    {
+        m_renderer.sprite = facing_down;
+    }
 
     void OnTriggerStay(Collider other)
      {
@@ -27,6 +47,16 @@ public class Player : MonoBehaviour
     {
         float x = Input.GetAxisRaw(inputAxisH);
         float y = Input.GetAxisRaw(inputAxisV);
+
+        if (x > 0)
+            m_renderer.sprite = facing_right;
+        if (x < 0)
+            m_renderer.sprite = facing_left;
+        if (y > 0)
+            m_renderer.sprite = facing_up;
+        if (y <= 0)
+            m_renderer.sprite = facing_down;
+
         Vector2 direction = new Vector2(x * speed, y * speed);
         transform.Translate(direction);
     }
