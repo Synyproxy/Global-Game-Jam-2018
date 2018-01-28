03c4eaa19a49bd90420d4c114139144c0bf30237
Elouan <elouankleg@hotmail.fr>
2018-01-27 22:13:23 -0500

    Adding SceneManaging with menu and credits

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadNewScene()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {        
+        Application.Quit();
+    }
+
+    public void LoadCreditScene()
+    {
+        SceneManager.LoadScene("Crédits", LoadSceneMode.Single);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+}
